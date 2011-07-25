e5be0d6ed78d5a7d84a67a16c174617b2e1aa90e
Gaurav <gaurav.sinha@sv.cmu.edu>
2011-07-24 18:12:49 -0700

    simple test of collection add and toString for printing.

--- src/testpkg/TestMain.java
+++ src/testpkg/TestMain.java
@@ -1,13 +1,22 @@
 package testpkg;
+import java.util.*;
 
 public class TestMain {
 
-	/**
-	 * @param args
-	 */
+	public static void fill(Collection c)
+	{
+		c.add("Hello");
+		c.add("How");
+		c.add("Are");
+		c.add("You");		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		CollectionTest.display_list();
+		//CollectionTest.display_list();
+	    Collection cute = new ArrayList<String>();	
+	    fill(cute);
+	    System.out.print(cute.toString());
 	}
 
 }
